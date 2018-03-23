0d58d8b0ae3d00af1c28e2d4826708d2ddc4bca3
Vlad Yaremenko <vladyaremenko95.vy@gmail.com>
2018-03-23 12:02:25 +0200

    Update bootstrap.sh file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,13 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
-
+# Update dotfiles
+echo "Update dotfiles..."
 git pull origin master;
 
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
-}
+echo "Update system..."
+sudo apt-get update        # Fetches the list of available updates
+sudo apt-get upgrade       # Strictly upgrades the current packages
+sudo apt-get dist-upgrade  # Installs updates (new ones)
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+echo "Install system dependencies..."
+sudo apt install git vim vim-gnome curl
