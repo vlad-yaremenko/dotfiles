ff86eb2995e949b796b3388f16fdee9b769892c6
Vlad Yaremenko <vladyaremenko95.vy@gmail.com>
2018-04-24 12:53:23 +0300

    Update bootstrap.sh file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,32 +1,83 @@
 #!/usr/bin/env bash
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
-echo "Update system..."
+echo ""
+echo -e "${RED}Update system...${NC}"
+echo ""
 sudo apt-get update        # Fetches the list of available updates
 sudo apt-get upgrade       # Strictly upgrades the current packages
 sudo apt-get dist-upgrade  # Installs updates (new ones)
 
-echo "Install system dependencies..."
-sudo apt-get -y install \
+echo ""
+echo -e "${RED}Install system dependencies...${NC}"
+echo ""
+sudo apt-get install --yes \
   git \
   vim \
   vim-gnome \
-  curl \
-  ack-grep
+  curl
 
-echo "Install slack..."
-sudo snap install -y slack --classic
+echo ""
+echo -e "${RED}Install slack...${NC}"
+echo ""
+sudo snap install slack --classic
 
-echo "Install skype..."
+echo ""
+echo -e "${RED}Install skype...${NC}"
+echo ""
 sudo apt-get install -y apt-transport-https
 curl https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
 echo "deb https://repo.skype.com/deb stable main" | sudo tee /etc/apt/sources.list.d/skypeforlinux.list
 sudo apt-get update
-sudo apt-get install -y skypeforlinux
+sudo apt-get install --yes skypeforlinux
 
-echo "Install chrome..."
+echo ""
+echo -e "${RED}Install chrome...${NC}"
+echo ""
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update
-sudo apt-get install -y google-chrome-stable
+sudo apt-get install --yes google-chrome-stable
 
-echo "Done..."
+echo ""
+echo -e "${RED}Install node.js...${NC}"
+echo ""
+curl -sL https://deb.nodesource.com/setup_9.x | sudo bash -
+sudo apt install nodejs
+
+echo ""
+echo -e "${RED}Create projects directory${NC}"
+echo ""
+mkdir ~/Projects
+
+echo ""
+echo -e "${RED}Clone dotfiles project${NC}"
+echo ""
+cd ~/Projects
+git clone https://github.com/vlad-yaremenko/dotfiles.git
+
+echo ""
+echo -e "${RED}Load vim submodules${NC}"
+echo ""
+cd ~/Projects/dotfiles
+git submodule update --init
+
+echo ""
+echo -e "${RED}Create symbol links${NC}"
+echo ""
+
+echo -e "${RED}For .vim folder${NC}"
+ln -s ~/Projects/dotfiles/.vim ~/.vim
+echo -e "${RED}For .vimrc${NC}"
+ln -s ~/Projects/dotfiles/.vimrc ~/.vimrc
+echo -e "${RED}For .gvimrc${NC}"
+ln -s ~/Projects/dotfiles/.gvimrc ~/.gvimrc
+echo -e "${RED}For .editorconfig${NC}"
+ln -s ~/Projects/dotfiles/.editorconfig ~/.editorconfig
+echo -e "${RED}For .gitconfig${NC}"
+ln -s ~/Projects/dotfiles/.gitconfig ~/.gitconfig
+
+echo ""
+echo -e "${RED}Done...${NC}"
+echo ""
