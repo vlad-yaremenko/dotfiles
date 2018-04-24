ca220a285be53e2060676d0e83cecac98f1911ae
Vlad Yaremenko <12101950+vlad-yaremenko@users.noreply.github.com>
2018-04-24 11:17:40 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-# Update dotfiles
-echo "Update dotfiles..."
-git pull origin master;
-
 echo "Update system..."
 sudo apt-get update        # Fetches the list of available updates
 sudo apt-get upgrade       # Strictly upgrades the current packages
 sudo apt-get dist-upgrade  # Installs updates (new ones)
 
 echo "Install system dependencies..."
-sudo apt-get install \
+sudo apt-get -y install \
   git \
   vim \
   vim-gnome \
@@ -18,17 +14,19 @@ sudo apt-get install \
   ack-grep
 
 echo "Install slack..."
-sudo snap install slack --classic
+sudo snap install -y slack --classic
 
 echo "Install skype..."
-sudo apt-get install apt-transport-https
+sudo apt-get install -y apt-transport-https
 curl https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
 echo "deb https://repo.skype.com/deb stable main" | sudo tee /etc/apt/sources.list.d/skypeforlinux.list
 sudo apt-get update
-sudo apt-get install skypeforlinux
+sudo apt-get install -y skypeforlinux
 
 echo "Install chrome..."
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update
-sudo apt-get install google-chrome-stable
+sudo apt-get install -y google-chrome-stable
+
+echo "Done..."
